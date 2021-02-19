4f399adccc9f06c15ec54119ff23a83f3850012a
Rupam <www.rupamdasgupta@gmail.com>
2021-02-19 19:16:32 +0530

    stupid bugs fixed

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from flask import Flask , render_template , request , redirect , url_for , session , flash
 from dbcheck import *
-from passlib.hash import pbkdf2_sha2
+from passlib.hash import pbkdf2_sha256
 import datetime
 from email_verifier import *
 import random
